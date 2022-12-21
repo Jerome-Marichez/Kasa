9686e01973654372b44f2dd890135e11186420ab
Jerome Marichez <jeromemarichez@ik.me>
2022-12-21 21:11:44 +0100

    Add link to Thumb
    Add link to thumb

--- kasa/src/components/Thumb/Thumb.jsx
+++ kasa/src/components/Thumb/Thumb.jsx
@@ -1,11 +1,15 @@
 import "./Thumb.scss";
-
+import { Link } from "react-router-dom";
 
 export default function Thumb({ id, title, img }) {
 
 	return (
-		<div className="thumb" style={{ backgroundImage: `url(${img})` }} onClick={() => { console.log("clicked") }}>
-			<h3>{title}</h3>
-		</div>
+
+		<Link to={`/logement/?id=${id}`}>
+			<div className="thumb" style={{ backgroundImage: `url(${img})` }} onClick={() => { console.log("clicked") }}>
+				<h3>{title}</h3>
+			</div>
+		</Link>
+
 	);
 }
