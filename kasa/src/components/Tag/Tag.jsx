56d527f801967c38e2e81f93a7c4418ec5b05ecf
Jerome Marichez <jeromemarichez@ik.me>
2023-01-03 23:31:52 +0100

    Tag Name
    Added tag name to the project

---
+++ kasa/src/components/Tag/Tag.jsx
@@ -0,0 +1,7 @@
+import "./Tag.scss";
+
+export default function Tag({title}) {
+	return (
+		<div className="tag"><p>{title}</p></div>
+	);
+}
--- kasa/src/pages/LogementPage.jsx
+++ kasa/src/pages/LogementPage.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import Carrousel from "../components/Carrousel/Carrousel";
 import Footer from "../components/Footer/Footer";
 import Dropdown from "../components/Dropdown/Dropdown";
+import Tag from "../components/Tag/Tag";
 import "./_pages.scss";
 import getData from "../utils/getData";
 import { useEffect, useState } from "react";
@@ -19,6 +20,9 @@ export function LogementPage() {
 	const [pictures, setPictures] = useState(0);
 	const [description, setDescription] = useState(0);
 	const [equipments, setEquipments] = useState(0);
+
+
+	const title = "test";
 
 
 	useEffect(() => {
@@ -43,6 +47,13 @@ export function LogementPage() {
 		<>
 			<Header />
 			<Carrousel pictures={pictures} />
+
+			<div className="container-tag">
+				<Tag title="title" />
+				<Tag title="d" />
+				<Tag title="b" />
+			</div>
+			
 			<div className="container-dropdown">
 				<Dropdown key="1" title="Description" texte={description} />
 				<Dropdown key="2" title="Équipements" texte={equipments} />
