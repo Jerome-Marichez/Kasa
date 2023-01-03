4241062454f06f11e4f51592b6e294e6ab1258bd
Jerome Marichez <jeromemarichez@ik.me>
2023-01-03 22:52:11 +0100

    Thumb remove console.log

--- kasa/src/components/Thumb/Thumb.jsx
+++ kasa/src/components/Thumb/Thumb.jsx
@@ -6,7 +6,7 @@ export default function Thumb({ id, title, img }) {
 	return (
 
 		<Link to={`/logement/?id=${id}`}>
-			<div className="thumb" style={{ backgroundImage: `url(${img})` }} onClick={() => { console.log("clicked") }}>
+			<div className="thumb" style={{ backgroundImage: `url(${img})` }}>
 				<h3>{title}</h3>
 			</div>
 		</Link>
