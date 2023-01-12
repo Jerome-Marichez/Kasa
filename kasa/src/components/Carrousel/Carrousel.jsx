75c5daced79d29d0810a56fc79f23a325235b88c
Jerome Marichez <jeromemarichez@ik.me>
2023-01-12 14:25:58 +0100

    Fix Carrousel Behavior
    I've misunderstand the behavior expected.
    Infact in this project the carrousel expect to be a infinite carrousel if a array of pictures is provide.
    
    So written the new code to fit the need.

--- kasa/src/components/Carrousel/Carrousel.jsx
+++ kasa/src/components/Carrousel/Carrousel.jsx
@@ -5,24 +5,36 @@ export default function Carrousel({ pictures }) {
 
 
 	const [index, setIndex] = useState(0);
-	const handlePrevious = () => setIndex(Math.max(index - 1, 0));
-	const handleNext = () => setIndex(Math.min(index + 1, pictures.length - 1));
+	const maxPicture = pictures.length - 1;
+
+
+	const handlePrevious = () => {
+		index == 0 ? setIndex(maxPicture) : setIndex(index - 1)
+	};
+	const handleNext = () => {
+		index == maxPicture ? setIndex(0) : setIndex(index + 1)
+	};
 
 	return (
 
 		<div className="carrousel" style={{ backgroundImage: `url(${pictures[index]})` }}>
 
-			<div className="container-select">
-				<div className={index !== 0 ? "previous show" : "previous hide"}
-					onClick={handlePrevious}>Previous
+			{pictures.length !== 1 ?
+				<div className="container-select">
+					<div className="previous"
+						onClick={handlePrevious}>Previous
+					</div>
+					<div className="next"
+						onClick={handleNext}> Next
+					</div>
+					<p>{index + 1}/{pictures.length}</p>
 				</div>
+				: ""
+			}
 
 
-				<div className={index + 1 !== pictures.length ? "next show" : "next hide"}
-					onClick={handleNext}> Next
-				</div>
-			</div>
-			<p>{index + 1}/{pictures.length /*Since a array start with [0] we show the index +1 */}</p>
+
+
 		</div >
 	);
 }
